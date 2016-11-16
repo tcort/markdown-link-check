d30853d7e62c7c15dd58e1a27adab9add1fafaae
slythfox <slythfox@users.noreply.github.com>
2016-11-16 15:32:40 -0700

    Fixed relative link checking for local input files.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var _ = require('lodash');
 var async = require('async');
 var linkCheck = require('link-check');
 var markdownLinkExtractor = require('markdown-link-extractor');
+var isRelativeUrl = require('is-relative-url');
+var fs = require('fs');
+var url = require('url');
 
 module.exports = function markdownLinkCheck(markdown, opts, callback) {
     if (arguments.length === 2 && typeof opts === 'function') {
@@ -12,7 +15,24 @@ module.exports = function markdownLinkCheck(markdown, opts, callback) {
         opts = {};
     }
 
+    var isSourceRelative = isRelativeUrl(opts.baseUrl)
+
     async.mapLimit(_.uniq(markdownLinkExtractor(markdown)), 2, function (link, callback) {
-        linkCheck(link, opts, callback);
+        if (isSourceRelative && isRelativeUrl(link)) {
+            var resolvedLink = url.resolve(opts.baseUrl, link)
+
+            fs.access(resolvedLink, fs.constants.F_OK, function (err) {
+              var result = {};
+              result.link = resolvedLink;
+              result.statusCode = (err === null) ? 200 : 404;
+              result.err = err;
+              result.status = (err === null) ? 'alive' : 'dead';
+
+              callback(err, result);
+            });
+        }
+        else {
+            linkCheck(link, opts, callback);
+        }
     }, callback);
 };
